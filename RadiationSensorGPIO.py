b25379e9cc97e1fe047f0a9553f311eac7aa9eb9
Ali Hanks <alihanks@gmail.com>
2021-10-20 18:05:37 -0700

    Oct 27

--- RadiationSensorGPIO.py
+++ RadiationSensorGPIO.py
@@ -1,10 +1,13 @@
 import RPi.GPIO as GPIO  
-from time import sleep     # this lets us have a time delay 
+import time     # this lets us have a time delay 
 import sys
+import csv
+
 
 count = 0
 
-def callback_final(detect_time):  
+def callback_final(interval, detect_time, delay, title):
+  header = ['Time', '# of Counts Detected within Interval', 'CPM within Interval']
   GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
   GPIO.setup(17, GPIO.IN)    # set 3V3 as input (button)  
     
@@ -14,25 +17,70 @@ def callback_final(detect_time):
     if GPIO.input(17):     # if pin 17 == 1
       global count
       count += 1
-      print(str(count) + ": Rising edge detected on 3V3") 
-    
+      #print(str(count) + ": Rising edge detected on 3V3") 
   
   
   # when a changing edge is detected on pin 17, regardless of whatever   
   # else is happening in the program, the function my_callback will be run  
   GPIO.add_event_detect(17, GPIO.RISING, callback=my_callback)  
   
-  sleep(detect_time)         # wait 30 seconds  
-   
-  print("\n")
-  print(str(count) + " counts detected in total") 
+  times = []
+  counts_int = []  
+  CPM_int = []
   
-  CPS = count/detect_time
-  print(str(CPS) + " Counts per Second")    
+  time.sleep(delay)
+  
+  start_time = time.time()
+  current_time = time.time()
+  
+  #sleep(detect_time)         # wait 30 seconds  
+   
+   
+     #print("\n")
+  #print(str(count) + " counts detected in total") 
+  
+  #print(str(CPS) + " Counts per Second")
+  
+  with open(title,'w', newline = '') as csvfile:
+    writer = csv.writer(csvfile, delimiter = ',')
+    writer.writerow(header)
+    while current_time < start_time + detect_time:
+      global count
+      count = count
+      
+      interval_count = count
+      interval_CPM = interval_count / (interval/60)
+      current_time = time.time() 
+      
+      times.append(current_time)
+      counts_int.append(interval_count)
+      CPM_int.append(interval_CPM)
+      
+      count = 0
+            
+      writer.writerow([current_time, interval_count, interval_CPM])
+      time.sleep(interval)
+      
+      print(current_time, interval_count, interval_CPM)
+      
+      
+    print()
+    print("\n")
+    
+    tot_counts = sum(counts_int)
+    print("Total Counts = " + str(tot_counts))
+    min_int = detect_time / 60
+    print("Overall CPM = " + str(tot_counts/min_int))
+  
+      
     
   #finally:                   # this block will run no matter how the try block exits  
   GPIO.cleanup()         # clean up after yourself  
-  
-detect_time= int(sys.argv[1])
-callback_final(detect_time)
+
+interval = int(sys.argv[1]) * 60
+detect_time = int(sys.argv[2]) * 60
+delay = int(sys.argv[3]) * 60
+title = sys.argv[4]
+
+callback_final(interval, detect_time, delay, title)
   
